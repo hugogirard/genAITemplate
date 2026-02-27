0f835403548956116132a33387a6ab1cffd57374
Hugo Girard <hugo.girard@microsoft.com>
2026-02-27 11:46:48 -0500

    added chat component

--- genAITemplate/src/web/src/app/app.ts
+++ genAITemplate/src/web/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../components/header/header';
 import { Sidebar } from '../components/sidebar/sidebar';
+import { Chat } from '../components/chat/chat';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidebar],
+  imports: [RouterOutlet, Header, Sidebar, Chat],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ genAITemplate/src/web/src/components/chat/chat.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'chat',
+    standalone: true,
+    templateUrl: './Chat.html',
+    styleUrl: './Chat.css'
+})
+export class Chat { }
